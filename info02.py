12b807974066b308f00fd4512e10169cf4b655b2
judah <judah.periyasamy@eduvaud.ch>
2024-01-17 23:00:03 +0100

    correction about the username in each game

--- info02.py
+++ info02.py
@@ -15,11 +15,11 @@ import datetime
 from tkinter.messagebox import *
 from database import *
 
-#important data (to save)
-pseudo="Gaston" #provisory pseudo for user
-exercise="INFO02"
-nbtrials=0 #number of total trials
-nbsuccess=0 #number of successfull trials
+# important data (to save)
+pseudo = "Gaston"  # provisory pseudo for user
+exercise = "INFO02"
+nbtrials = 0  # number of total trials
+nbsuccess = 0  # number of successfull trials
 
 # Liaison entre le canvas et le code
 unite = ["B", "kB", "MB", "GB", "TB"]
@@ -51,34 +51,29 @@ def next(event):
     entry_n2.delete(0, 'end')
 
 
-def save_game(event):
+def save_game(event, username):
     # TODO
-    open_dbconnection()
+    database.open_dbconnection()
     global entry_pseudo, duration
     title = 2
-    username = entry_pseudo.get()
-    if username == "":
-        print("TEST")
-        messagebox.showinfo("Message", "Veuillez entrez un pseudo!!")
-    else:
-        try:
-           res = add_results(username, duration ,nbsuccess, nbtrials, title)
-           if res:
-               print("dans save")
-           else:
-                print("Echec de l'ajout.\n")
-        except Exception:
+    try:
+        res = database.add_results(username, duration, nbsuccess, nbtrials, title)
+        if res:
+            messagebox.showinfo("Message", "Exo Fini !!!")
+            print("dans save")
+        else:
             print("Echec de l'ajout.\n")
-
-    close_dbconnection()
+    except Exception:
+        print("Echec de l'ajout.\n")
+    database.close_dbconnection()
 
 
 def test(event):
     global n2, nbsuccess, nbtrials
     # Fonction pour tester si la valeur est juste
     n2 = float(entry_n2.get().replace(" ", ""))
-    nbtrials+=1
-    success = (abs(n1 / n2 / rapport - 1) < 0.01) #tolerance 1%
+    nbtrials += 1
+    success = (abs(n1 / n2 / rapport - 1) < 0.01)  # tolerance 1%
     if success:
         nbsuccess += 1
         window_info02.configure(bg="green")
@@ -86,7 +81,7 @@ def test(event):
         window_info02.configure(bg="red")
     lbl_result.configure(text=f"Essais réussis : {nbsuccess} / {nbtrials}")
     window_info02.update()
-    time.sleep(1) # delai 1s
+    time.sleep(1)  # delai 1s
     next(event=None)
 
 
@@ -103,40 +98,39 @@ def open_window_info_02(window, username):
     global window_info02, lbl_duration, lbl_result, entry_n2, label_u2, label_n1, hex_color, start_date, entry_pseudo
     window_info02 = tk.Toplevel(window)
 
-    #window_info02 = tk.Tk()
+    # window_info02 = tk.Tk()
     window_info02.title("Conversion d'unités")
     window_info02.geometry("1100x900")
-    window_info02.grid_columnconfigure((0,1,2), minsize=150, weight=1)
+    window_info02.grid_columnconfigure((0, 1, 2), minsize=150, weight=1)
 
     # color definition
     rgb_color = (139, 201, 194)
-    hex_color = '#%02x%02x%02x' % rgb_color # translation in hexa
+    hex_color = '#%02x%02x%02x' % rgb_color  # translation in hexa
     window_info02.configure(bg=hex_color)
 
     lbl_title = tk.Label(window_info02, text=f"{exercise}", font=("Arial", 15))
-    lbl_title.grid(row=0,column=0,columnspan=3, ipady=5, padx=20,pady=20)
+    lbl_title.grid(row=0, column=0, columnspan=3, ipady=5, padx=20, pady=20)
     lbl_duration = tk.Label(window_info02, text="0:00", font=("Arial", 15))
-    lbl_duration.grid(row=0,column=2, ipady=5, padx=10,pady=10)
+    lbl_duration.grid(row=0, column=2, ipady=5, padx=10, pady=10)
 
     tk.Label(window_info02, text='Pseudo:', font=("Arial", 15)).grid(row=1, column=0, padx=5, pady=5)
     lbl_pseudo = tk.Label(window_info02, text=username, font=("Arial", 15))
     lbl_pseudo.grid(row=1, column=1)
 
-
     lbl_result = tk.Label(window_info02, text=f"{pseudo}  Essais réussis : 0/0", font=("Arial", 15))
-    lbl_result.grid( row=1, column=2,columnspan=3, ipady=5, padx=20,pady=20)
+    lbl_result.grid(row=1, column=2, columnspan=3, ipady=5, padx=20, pady=20)
 
-    label_n1 = tk.Label(window_info02, text="n1:",font=("Arial", 15))
-    label_n1.grid(row=2,column=0,ipady=5, padx=20,pady=20,sticky='E')
+    label_n1 = tk.Label(window_info02, text="n1:", font=("Arial", 15))
+    label_n1.grid(row=2, column=0, ipady=5, padx=20, pady=20, sticky='E')
 
-    entry_n2 = tk.Entry(window_info02,font=("Arial", 15))
-    entry_n2.grid(row=2,column=1,ipady=5,padx=5, pady=20,sticky='E')
+    entry_n2 = tk.Entry(window_info02, font=("Arial", 15))
+    entry_n2.grid(row=2, column=1, ipady=5, padx=5, pady=20, sticky='E')
 
-    label_u2 =tk.Label(window_info02, text="u2:",font=("Arial", 15))
-    label_u2.grid(row=2,column=2,ipady=5,padx=5,pady=20,sticky='W')
+    label_u2 = tk.Label(window_info02, text="u2:", font=("Arial", 15))
+    label_u2.grid(row=2, column=2, ipady=5, padx=5, pady=20, sticky='W')
 
-    btn_next =tk.Button(window_info02, text="Suivant", font=("Arial", 15))
-    btn_next.grid(row=3,column=0,columnspan=3,ipady=5, padx=5,pady=5)
+    btn_next = tk.Button(window_info02, text="Suivant", font=("Arial", 15))
+    btn_next.grid(row=3, column=0, columnspan=3, ipady=5, padx=5, pady=5)
 
     btn_finish = tk.Button(window_info02, text="Terminer", font=("Arial", 15))
     btn_finish.grid(row=6, column=0, columnspan=6)
@@ -149,7 +143,7 @@ def open_window_info_02(window, username):
     # binding actions (entry & buttons)
     entry_n2.bind("<Return>", test)
     btn_next.bind("<Button-1>", next)
-    btn_finish.bind("<Button-1>", save_game)
+    btn_finish.bind("<Button-1>", lambda e: save_game(e, username))
 
     # Main loop
     window_info02.mainloop()
