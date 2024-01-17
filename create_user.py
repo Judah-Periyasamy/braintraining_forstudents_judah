dc2fd9cd0b8e5f82986afb8618db75a64cff5d12
judah <judah.periyasamy@eduvaud.ch>
2024-01-17 23:05:40 +0100

    confirmation password

--- create_user.py
+++ create_user.py
@@ -6,35 +6,43 @@ from passlib.hash import bcrypt
 
 def show():
     entry_new_pass.configure(show='')
+    entry_confirm_pass.configure(show='')
     check.configure(command=hide, text='hide password')
 
 
 def hide():
     entry_new_pass.configure(show='*')
+    entry_confirm_pass.configure(show='*')
     check.configure(command=show, text='show password')
 
 
 def create_user_window():
-    global entry_new_pass, check
+    global entry_new_pass, entry_confirm_pass, check
 
     def create_user_action():
         new_username = entry_new_username.get()
         new_password = entry_new_pass.get()
+        confirm_password = entry_confirm_pass.get()
+
+        # Vérifier si les mots de passe correspondent
+        if new_password != confirm_password:
+            messagebox.showerror("Erreur", "Les mots de passe ne correspondent pas.")
+            return
 
         # Perform validation on the input if needed
 
         # Create the new user
         create_user(new_username, new_password)
         print(f"Utilisateur créé avec le nom {new_username}")
-        messagebox.showinfo("Creation réussie", f"Utilisateur créé avec le nom {new_username}")
+        messagebox.showinfo("Création réussie", f"Utilisateur créé avec le nom {new_username}")
 
-        # Close the create user window
+        # Fermer la fenêtre de création d'utilisateur
         create_user_window.destroy()
 
     create_user_window = tk.Tk()
 
     create_user_window.title("Créer un utilisateur")
-    create_user_window.geometry("500x300")
+    create_user_window.geometry("500x350")  # Ajusté pour la confirmation de mot de passe
 
     rgb_color = (139, 201, 194)
     hex_color = '#%02x%02x%02x' % rgb_color
@@ -48,9 +56,11 @@ def create_user_window():
 
     lbl_new_username = tk.Label(infos_frame, text="Nom d'utilisateur: ", bg="white", padx=40, font=("Arial,11"))
     lbl_new_password = tk.Label(infos_frame, text="Mot de passe: ", bg="white", padx=40, font=("Arial,11"))
+    lbl_confirm_password = tk.Label(infos_frame, text="Confirmer le mot de passe: ", bg="white", padx=40, font=("Arial,11"))
 
     entry_new_username = tk.Entry(infos_frame)
     entry_new_pass = tk.Entry(infos_frame, show="*")
+    entry_confirm_pass = tk.Entry(infos_frame, show="*")
 
     check = tk.Checkbutton(infos_frame, text='Afficher le mot de passe', command=show)
     btn_create_user = tk.Button(infos_frame, text="Créer utilisateur", command=create_user_action)
@@ -63,7 +73,10 @@ def create_user_window():
     lbl_new_password.grid(row=1, column=0, padx=(0, 10))
     entry_new_pass.grid(row=1, column=1)
 
-    check.grid(row=3, column=0, columnspan=2)
-    btn_create_user.grid(row=4, column=1, pady=10)
+    lbl_confirm_password.grid(row=2, column=0, padx=(0, 10))
+    entry_confirm_pass.grid(row=2, column=1)
+
+    check.grid(row=4, column=0, columnspan=2)
+    btn_create_user.grid(row=5, column=1, pady=10)
 
     create_user_window.mainloop()
